1ffdb25a638d0f2b32c8c3bee090232cafdec039
Hanjoony <sh00go@naver.com>
2021-12-22 15:38:59 +0900

    삼각형 넓이 구하기 추가

--- main.cpp
+++ main.cpp
@@ -6,5 +6,23 @@ int main()
 {
 	cout << "Hello World" << endl;
 
+
+
+	int Height = 0;
+	int Bottom = 0;
+
+	float Triangle = 0;
+
+	cin >> Height;
+	cin >> Bottom;
+
+	Triangle = static_cast<float>(Height) * static_cast<float>(Bottom) / 2.0f;
+
+
+	cout << "»ï°¢ÇüÀÇ ³ÐÀÌ´Â" << endl;
+
+	cout << Triangle << endl;
+
+
 	return 0;
 }
